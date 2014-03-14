79bfc0a5bcd11b2fd0eadf4efb974254bd8c9c73
gauden <workersgae@gmail.com>
2014-03-14 12:41:06 +0100

    Update the creative commons link

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 """
-Script to add creative commons license 
-to the top of each HTML page, and to remove
-the outdated link to the UNSW virtual library.
+Script to edit ldb.org website seeking an arbitrary
+NEEDLE and replacing it with REPLACEMENT; both being
+global variables set at the top of the script.
 
 by: Gauden Galea http://gaudengalea.com
-date: 25-Jan-2014
+date: 14-Mar-2014
 license: Public Domain
 """
 
@@ -12,10 +12,9 @@ from path import path
 
 DEBUG = True
 
-NEEDLE = 'The <a href="http://sphcm.med.unsw.edu.au/sphcm.nsf/website/forstudents.resources.ldb">public health virtual library</a> is now updated and hosted by the UNSW School of Public Health.'
+NEEDLE = '<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US"><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Website</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="ldb.org" property="cc:attributionName" rel="cc:attributionURL">Eberhard Wenzel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US">Creative Commons Attribution-NonCommercial 4.0 International License</a>'
 
-REPLACEMENT = '<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US"><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Website</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="ldb.org" property="cc:attributionName" rel="cc:attributionURL">Eberhard Wenzel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US">Creative Commons Attribution-NonCommercial 4.0 International License</a>.'
-
+REPLACEMENT = '<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US"><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Website</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://ldb.org" property="cc:attributionName" rel="cc:attributionURL">Eberhard Wenzel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.en_US">Creative Commons Attribution-NonCommercial 4.0 International License</a>'
 
 LDB = path('..')
 
